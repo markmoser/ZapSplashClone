fd0fb690e01304aba1a8ad56d8a364852e98dae7
Marissa Moser <marissalmoser@gmail.com>
2023-04-12 21:57:46 -0500

    enemy slider works

--- TylerMarissa/Assets/scripts/GameManager.cs
+++ TylerMarissa/Assets/scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,11 +13,19 @@ public class GameManager : MonoBehaviour
     private int buttonsPressed = 0;
     public int EnemiesKilled = 0;
     private int prisonersFreed = 0;
+
+    [SerializeField] private Slider enemiesKilledSlider;
     void Start()
     {
         PlayerInput.Instantiate(ElePlayer, 0, null, pairWithDevice: Gamepad.all[0]);
         //PlayerInput.Instantiate(WaterPlayer, 1, null, pairWithDevice: Gamepad.all[1]);
     }
+
+    private void Update()
+    {
+        enemiesKilledSlider.value = EnemiesKilled;
+    }
+
     public void CountButton()
     {
         buttonsPressed++;
