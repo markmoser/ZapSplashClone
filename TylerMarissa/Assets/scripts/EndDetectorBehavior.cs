4a535de7cda7815599d6e4f7e4ba974e1f80b806
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2023-04-18 12:08:28 -0500

    Added and end scrreen
    idk

---
+++ TylerMarissa/Assets/scripts/EndDetectorBehavior.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndDetectorBehavior : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player") {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
