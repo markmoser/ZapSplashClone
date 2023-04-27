0c0a778deb57c33cd4cec24f85077da33e3a9ec7
TylerBouchard <117851242+TylerBouchard@users.noreply.github.com>
2023-04-27 04:46:19 -0500

    Some Progress

--- TylerMarissa/Assets/scripts/GameManager.cs
+++ TylerMarissa/Assets/scripts/GameManager.cs
@@ -58,7 +58,7 @@ public class GameManager : MonoBehaviour
         buttonsPressedSlider.value = buttonsPressed;
         npcsFreedSlider.value = prisonersFreed;
 
-        if (prisonersFreed >= 8 && buttonsPressed >= 4 && EnemiesKilled >= 6)
+        if (prisonersFreed >= 8 && buttonsPressed >= 4 && EnemiesKilled >= 12)
         {
             exitDoorScript.OpenDoor();
         }
